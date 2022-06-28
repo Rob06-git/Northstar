ea1c67fef2573d4ca816b971a7580f6d6ceebae7
Rob06-git <99805558+Rob06-git@users.noreply.github.com>
2022-06-28 16:11:12 +0530

    Delete kubeconfig.tf

--- kubeconfig.tf
+++
@@ -1,8 +0,0 @@
-resource "null_resource" "kubectl" {
-depends_on = [
-    resource.aws_iam_role.eks_cluster
-]    
-provisioner "local-exec" {
-command = "aws eks --region eu-west-1 update-kubeconfig --name NS_cluster"
-}
-}
